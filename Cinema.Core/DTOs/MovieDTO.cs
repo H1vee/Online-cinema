530a44fbf00fe8a822778dd9e612eb4a7641106e
Mykytiuk-Anatolii <124164311+Mykytiuk-Anatolii@users.noreply.github.com>
2025-01-29 18:38:36 +0200

    Update MovieDTO.cs
    Change float to double,and other little changes

--- Cinema.Core/DTOs/MovieDTO.cs
+++ Cinema.Core/DTOs/MovieDTO.cs
@@ -6,6 +6,9 @@ namespace Cinema.Core.DTOs
         public string Title { get; set; } =string.Empty;
         public string Description { get; set; } =string.Empty;
         public DateTime ReleaseDate { get; set; }
-        public Double Rating { get; set; }
+        public float? Rating { get; set; }
+        public TimeSpan Duration { get; set; }
+        public List<string> Genres { get; set; } = new();
+        public List<string> Actors { get; set; } = new();
     }
 }
