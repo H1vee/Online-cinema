eb8306cda75d8f81b6147cc7a81539c87a9d4cbb
H1vee <158073572+H1vee@users.noreply.github.com>
2025-01-31 11:17:21 +0200

    Update TicketDTO.cs
    Fixed problems with seats.

--- Cinema.Core/DTOs/TicketDTO.cs
+++ Cinema.Core/DTOs/TicketDTO.cs
@@ -2,12 +2,13 @@ namespace Cinema.Core.DTOs
 {
   public class TicketDTO
   {
-      public int Id { get; set; }
-      public string MovieTitle { get; set; }= string.Empty;
-      public DateTime ShowDateTime { get; set; }
-      public string SeatNumber { get; set; } = string.Empty;
-      public decimal FinalPrice { get; set; }
-      public string Status { get; set; } = string.Empty;
+        public int Id { get; set; }
+        public string MovieTitle { get; set; } = string.Empty;
+        public DateTime ShowDateTime { get; set; }
+        public int RowNumber { get; set; } 
+        public int SeatNumber { get; set; }
+        public decimal FinalPrice { get; set; }
+        public string Status { get; set; } = string.Empty;
   }  
 }
 
