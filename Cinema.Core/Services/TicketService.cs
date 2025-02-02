f5a963810ec0cf49e5d051513e9fd20ea787d01a
Mykytiuk-Anatolii <124164311+Mykytiuk-Anatolii@users.noreply.github.com>
2025-02-02 11:46:23 +0200

    Create TicketService.cs
    Create Ticket Service

---
+++ Cinema.Core/Services/TicketService.cs
@@ -0,0 +1,57 @@
+using AutoMapper;
+using Cinema.Core.DTOs;
+using Cinema.Core.Interfaces;
+using Cinema.Infrastructure.Entities;
+using Cinema.Infrastructure.UnitOfWork;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cinema.Core.Services
+{
+    public class TicketService : ITicketService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public TicketService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<TicketDTO>> GetAllTicketsAsync()
+        {
+            var tickets = await _unitOfWork.Tickets.GetAllAsync();
+            return _mapper.Map<IEnumerable<TicketDTO>>(tickets);
+        }
+
+        public async Task<TicketDTO?> GetTicketByIdAsync(int id)
+        {
+            var ticket = await _unitOfWork.Tickets.GetByIdAsync(id);
+            return ticket == null ? null : _mapper.Map<TicketDTO>(ticket);
+        }
+
+        public async Task AddTicketAsync(TicketDTO ticketDto)
+        {
+            var ticket = _mapper.Map<Ticket>(ticketDto);
+            await _unitOfWork.Tickets.AddAsync(ticket);
+            await _unitOfWork.CompleteAsync();
+        }
+
+        public async Task<bool> DeleteTicketAsync(int id)
+        {
+            var ticket = await _unitOfWork.Tickets.GetByIdAsync(id);
+            if (ticket == null) return false;
+
+            _unitOfWork.Tickets.Remove(ticket);
+            await _unitOfWork.CompleteAsync();
+            return true;
+        }
+
+        public async Task<IEnumerable<TicketDTO>> GetTicketsBySaleIdAsync(int saleId)
+        {
+            var tickets = await _unitOfWork.Tickets.GetTicketsBySaleId(saleId);
+            return _mapper.Map<IEnumerable<TicketDTO>>(tickets);
+        }
+    }
+}
