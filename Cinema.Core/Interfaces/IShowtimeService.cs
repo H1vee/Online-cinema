3c8757b53ef4dfb64775d55d55da5707ee8d923e
RomanovMykola1717 <162117283+RomanovMykola1717@users.noreply.github.com>
2025-02-03 13:30:22 +0200

    Add files via upload

---
+++ Cinema.Core/Interfaces/IShowtimeService.cs
@@ -0,0 +1,14 @@
+using Cinema.Core.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cinema.Core.Interfaces
+{
+	public interface IShowtimeService
+	{
+		Task<IEnumerable<ShowtimeDTO>> GetAllShowtimesAsync();
+		Task<ShowtimeDTO?> GetShowtimeByIdAsync(int id);
+		Task AddShowtimeAsync(ShowtimeDTO showtimeDto);
+		Task<bool> DeleteShowtimeAsync(int id);
+	}
+}
