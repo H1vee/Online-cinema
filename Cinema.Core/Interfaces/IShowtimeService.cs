ddf0166f2abd6c239603538bc94746c4dff4858a
RomanovMykola1717 <162117283+RomanovMykola1717@users.noreply.github.com>
2025-02-03 23:12:22 +0200

    Update IShowtimeService.cs

--- Cinema.Core/Interfaces/IShowtimeService.cs
+++ Cinema.Core/Interfaces/IShowtimeService.cs
@@ -4,11 +4,12 @@ using System.Threading.Tasks;
 
 namespace Cinema.Core.Interfaces
 {
-	public interface IShowtimeService
-	{
-		Task<IEnumerable<ShowtimeDTO>> GetAllShowtimesAsync();
-		Task<ShowtimeDTO?> GetShowtimeByIdAsync(int id);
-		Task AddShowtimeAsync(ShowtimeDTO showtimeDto);
-		Task<bool> DeleteShowtimeAsync(int id);
-	}
+    public interface IShowtimeService
+    {
+        Task<IEnumerable<ShowtimeDTO>> GetAllShowtimesAsync();
+        Task<ShowtimeDTO?> GetShowtimeByIdAsync(int id);
+        Task AddShowtimeAsync(ShowtimeDTO showtimeDto);
+        Task<bool> DeleteShowtimeAsync(int id);
+        Task<IEnumerable<ShowtimeDTO>> GetUpcomingShowtimesAsync();
+    }
 }
